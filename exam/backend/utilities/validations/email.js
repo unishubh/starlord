d1d2d65b18ca4140a1f672a37970873a2df178f1
KodesiaYash <yash.kodesia98@gmail.com>
2020-06-02 17:40:14 +0530

    Error Handlers

---
+++ exam/backend/utilities/validations/email.js
@@ -0,0 +1,21 @@
+const db = require('../../models') ;
+module.exports.register = async ( req , res ) => {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        const email = req.body.email ;
+        if(!re.test(email) || email == "" ){
+            res.status(400) ;
+            res.send({
+            "message" : "Wrong email id " 
+            }) ;
+        }
+        const found_entry =  await db.email_mapping.findByPk(email) ;
+        console.log ( 'Found entry _______' + found_entry ) ;
+        if ( found_entry ){
+            res.status(400) ;
+            res.send({
+            "message" : " User with this Email id exists " 
+            }) ;
+        }
+        else
+            console.log('Email id can be used to create account') ;
+}
