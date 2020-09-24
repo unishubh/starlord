cffd073afddcc44fac3d8a9cc71baf434b7707ee
riyakwl28 <riyakwl28@gmail.com>
2020-09-24 20:25:20 +0530

    eslint error removed

--- exam/backend/migrations/20200924192530-alter_subscriptions_table.js
+++ exam/backend/migrations/20200924192530-alter_subscriptions_table.js
@@ -2,51 +2,51 @@ module.exports = {
     up: async (queryInterface, Sequelize) => {
         return Promise.all([
             queryInterface.addColumn(
-              'subscriptions', // table name
-              'planId', // new field name
-              {
-                type: Sequelize.UUID,
-                allowNull: false,
-              },
+                'subscriptions', // table name
+                'planId', // new field name
+                {
+                    type: Sequelize.UUID,
+                    allowNull: false,
+                },
             ),
             queryInterface.addColumn(
-              'subscriptions',
-              'startDate',
-              {
-                type: Sequelize.DATE,
-                allowNull: false,
-              },
+                'subscriptions',
+                'startDate',
+                {
+                    type: Sequelize.DATE,
+                    allowNull: false,
+                },
             ),
             queryInterface.addColumn(
-              'subscriptions',
-              'endDate',
-              {
-                type: Sequelize.DATE,
-                allowNull: false,
-              },
+                'subscriptions',
+                'endDate',
+                {
+                    type: Sequelize.DATE,
+                    allowNull: false,
+                },
             ),
             queryInterface.addColumn(
                 'subscriptions',
                 'isExpired',
                 {
-                  type: Sequelize.BOOLEAN,
+                    type: Sequelize.BOOLEAN,
                 },
-              ),
-          ]);
+            ),
+        ]);
     },
-  
+
     down: async (queryInterface, Sequelize) => {
         return Promise.all([
             queryInterface.removeColumn('subscriptions', 'planId'),
             queryInterface.removeColumn('subscriptions', 'startDate'),
             queryInterface.removeColumn('subscriptions', 'endDate'),
             queryInterface.removeColumn('subscriptions', 'isExpired'),
-          ]);
-      /**
-       * Add reverting commands here.
-       *
-       * Example:
-       * await queryInterface.dropTable('users');
-       */
+        ]);
+        /**
+         * Add reverting commands here.
+         *
+         * Example:
+         * await queryInterface.dropTable('users');
+         */
     },
-  };
+};
--- exam/backend/models/subscriptions.js
+++ exam/backend/models/subscriptions.js
@@ -3,16 +3,16 @@ module.exports = (sequelize, DataTypes) => {
     id: { type: DataTypes.UUID, primaryKey: true },
     userID: { type: DataTypes.UUID, allowNull: false },
     examID: { type: DataTypes.UUID, allowNull: false },
-    planId:{type: DataTypes.UUID, allowNull:false},
-    startDate:{type:DataTypes.DATE,allowNull:false},
-    endDate:{type:DataTypes.DATE},allowNull:false,
-    isExpired:{type:DataTypes.BOOLEAN},
+    planId: { type: DataTypes.UUID, allowNull: false },
+    startDate: { type: DataTypes.DATE, allowNull: false },
+    endDate: { type: DataTypes.DATE }, allowNull: false,
+    isExpired: { type: DataTypes.BOOLEAN },
   });
   subscriptions.associate = function (models) {
     // associations can be defined here
     subscriptions.belongsTo(models.user, { foreignKey: "userID" });
     subscriptions.belongsTo(models.exams, { foreignKey: "examID" });
-    subscriptions.belongsTo(models.plans,{foreignKey:"planId"});
+    subscriptions.belongsTo(models.plans, { foreignKey: "planId" });
   };
   return subscriptions;
 };
